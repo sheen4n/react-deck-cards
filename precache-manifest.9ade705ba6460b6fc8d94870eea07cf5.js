e5730da74857d03d831e2760782ae500c22c997f
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-19 17:39:45 +0800

    Updates

---
+++ precache-manifest.9ade705ba6460b6fc8d94870eea07cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c2a596b73f70f2572860a99a8678a0",
+    "url": "/react-deck-cards/index.html"
+  },
+  {
+    "revision": "aff608784390d790d464",
+    "url": "/react-deck-cards/static/css/main.cea21e8d.chunk.css"
+  },
+  {
+    "revision": "740ba0bc7d952e89a889",
+    "url": "/react-deck-cards/static/js/2.1c9b6b80.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-deck-cards/static/js/2.1c9b6b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff608784390d790d464",
+    "url": "/react-deck-cards/static/js/main.18f5b62e.chunk.js"
+  },
+  {
+    "revision": "4d677574e06726d8f31d",
+    "url": "/react-deck-cards/static/js/runtime-main.22afe15c.js"
+  }
+]);
